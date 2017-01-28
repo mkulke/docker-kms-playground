599ea7cc17166575dcd267686933b58c1bfe8429
Magnus Kulke <magnus.kulke@moovel.com>
2017-01-28 15:30:13 +0100

    Erase Plaintext from env in init.sh.

--- init.sh
+++ init.sh
@@ -9,4 +9,6 @@ do
   export ${KEY%_ENCRYPTED}="$(printenv $KEY | base64 --decode | openssl aes-256-cbc -d -pass env:PASSPHRASE)"
 done < <(compgen -e | grep "_ENCRYPTED$")
 
+unset PASSPHRASE
+
 exec watch -n5 printenv MY_SECRET
